e3e3db0af99cf0c860a7992c9efe8e531eb9e672
spyboy11 <52691677+spyboy11@users.noreply.github.com>
2022-03-31 14:16:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/news_app/news_home.kt
@@ -0,0 +1,11 @@
+package com.example.news_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class news_home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_home)
+    }
+}
