f600e1150e884411e85d2fd525e1f271d8031439
spyboy11 <52691677+spyboy11@users.noreply.github.com>
2022-03-31 16:56:54 +0530

    mainActivity implementation

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.news_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.paging.PagedList
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.news_app.api_data.apiData
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        initRecyclerView()
+        initViewModel()
+    }
+
+    private fun initRecyclerView(){
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val decoration = DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL)
+            addItemDecoration(decoration)
+
+            recyclerViewAdapter = RecyclerViewAdapter()
+            adapter = recyclerViewAdapter
+        }
+    }
+
+    private fun initViewModel(){
+        val viewModelProvider = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModelProvider.getRecyclerListObserver()?.observe(this, Observer<PagedList<apiData>> {
+            if (it != null){
+                recyclerViewAdapter.submitList(it)
+            } else {
+                Toast.makeText(this@MainActivity,"Failed to Ftech Data",Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
